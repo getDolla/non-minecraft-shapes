cbb6ee8152fff0b6bc994c9c732735beb10dcb1d
Yikai <ywang8@stuy.edu>
2017-03-16 10:40:33 -0400

    finished add circle

--- draw.py
+++ draw.py
@@ -1,9 +1,21 @@
 from display import *
 from matrix import *
 
-
+#in x-y plane
 def add_circle( points, cx, cy, cz, r, step ):
-    pass
+    x_0 = r + cx
+    y_0 = cy
+
+    t = step
+    while t <= 1.0001:
+        x_1 = r * cos(2 * pi * t) + cx
+        y_1 = r * sin(2 * pi * t) + cy
+
+        add_edge(points, x_0, y_0, cz, x_1, y_1, cz)
+
+        x_0 = x_1
+        y_0 = y_1
+        t += step
 
 def add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
     pass
@@ -14,23 +26,23 @@ def draw_lines( matrix, screen, color ):
     if len(matrix) < 2:
         print 'Need at least 2 points to draw'
         return
-    
+
     point = 0
     while point < len(matrix) - 1:
         draw_line( int(matrix[point][0]),
                    int(matrix[point][1]),
                    int(matrix[point+1][0]),
                    int(matrix[point+1][1]),
-                   screen, color)    
+                   screen, color)
         point+= 2
-        
+
 def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
     add_point(matrix, x0, y0, z0)
     add_point(matrix, x1, y1, z1)
-    
+
 def add_point( matrix, x, y, z=0 ):
     matrix.append( [x, y, z, 1] )
-    
+
 
 
 
@@ -54,7 +66,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if ( abs(x1-x0) >= abs(y1 - y0) ):
 
         #octant 1
-        if A > 0:            
+        if A > 0:
             d = A + B/2
 
             while x < x1:
